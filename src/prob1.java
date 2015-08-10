b4a64a91e7b50dfd719e419c111bc03bdd04c82d
bmpasini <bmpasini@gmail.com>
2015-08-10 15:50:04 -0400

    solve problem number 1

---
+++ src/prob1.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+public class prob1 {
+	
+	public static void main(String[] args) { 
+		displaySumOfMultiples(1000);
+	}
+	
+	public static void displaySumOfMultiples(int iter) {
+		int sum = findSumOfMultiples(iter);
+		System.out.println(sum);
+	}
+	
+	public static int findSumOfMultiples(int iter) {
+		
+		ArrayList<Integer> multiples = new ArrayList<Integer>();
+		
+		for (int i = 1; i < iter; i++) {
+			if (isMultipleOfx(i,3) || isMultipleOfx(i,5)) {
+				multiples.add(i);
+			}
+		}
+		
+		return sumIntArr(multiples);
+		
+	}
+	
+	public static boolean isMultipleOfx(int n, int x) {
+		if (n % x == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static int sumIntArr(ArrayList<Integer> arr) {
+		int sum = 0;
+		for (int x : arr) {
+			sum += x;
+		}
+		return sum;
+	}
+
+}
